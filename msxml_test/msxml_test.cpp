29208702278a95fbc073672fbe371dc03c2bcd1d
artemyv <artemyv@gmail.com>
2022-05-13 11:47:28 +0300

    initial commit

--- msxml_test/msxml_test.cpp
+++ msxml_test/msxml_test.cpp
@@ -2,19 +2,25 @@
 //
 
 #include <iostream>
+#include <format>
+#include <source_location>
+#include <string_view>
 
+#define LOG(...) logline(std::source_location::current(), __VA_ARGS__)
+
+template<class... Args>
+void logline(const std::source_location location, const std::string_view fmt, const Args&... args)
+{
+    std::cout << std::format("{}:{} - ", location.file_name(), location.line()) << std::format(fmt, args...) << '\n';
+}
+template<class... Args>
+void logline(const std::source_location location, const std::wstring_view fmt, const Args&... args)
+{
+    std::format_to(std::ostream_iterator<char>(std::cout), "{}:{} - ", location.file_name(), location.line());
+    std::format_to(std::ostream_iterator<wchar_t>(std::wcout), fmt, args...);
+    std::cout << '\n';
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    LOG("Hello world");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
