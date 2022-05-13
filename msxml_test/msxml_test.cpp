7f5abdc66a41da2ba279f0bfb4911d3aa02a63c1
artemyv <artemyv@gmail.com>
2022-05-13 14:53:33 +0300

    Add format lib

--- msxml_test/msxml_test.cpp
+++ msxml_test/msxml_test.cpp
@@ -1,26 +1,13 @@
 // msxml_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <format>
-#include <source_location>
-#include <string_view>
+#pragma warning(disable:4189)
+#include <fmt/core.h>
+#pragma warning(default:4189)
 
-#define LOG(...) logline(std::source_location::current(), __VA_ARGS__)
-
-template<class... Args>
-void logline(const std::source_location location, const std::string_view fmt, const Args&... args)
-{
-    std::cout << std::format("{}:{} - ", location.file_name(), location.line()) << std::format(fmt, args...) << '\n';
-}
-template<class... Args>
-void logline(const std::source_location location, const std::wstring_view fmt, const Args&... args)
-{
-    std::format_to(std::ostream_iterator<char>(std::cout), "{}:{} - ", location.file_name(), location.line());
-    std::format_to(std::ostream_iterator<wchar_t>(std::wcout), fmt, args...);
-    std::cout << '\n';
-}
-int main()
-{
-    LOG("Hello world");
+int main(int argc, const char* argv[]) {
+	if (argc > 0) {
+		fmt::print("Hello from {}\n", argv[0]);
+	}
+	return 0;
 }
